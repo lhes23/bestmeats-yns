27f7a5c3be0fd94be08f0b7659e787e80b59bacf
lhes23 <32542078+lhes23@users.noreply.github.com>
2025-06-06 09:11:18 +0800

    biome check

--- src/app/(store)/[...segments]/page.tsx
+++ src/app/(store)/[...segments]/page.tsx
@@ -1,36 +1,36 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
-import Link, { type LinkProps } from 'next/link'
-import { notFound } from 'next/navigation'
+import { MDXRemote } from "next-mdx-remote/rsc";
+import Link, { type LinkProps } from "next/link";
+import { notFound } from "next/navigation";
 
 const pages: Record<string, { content: string }> = {
-  '/about-me': {
-    content: ``
-  }
-}
+	"/about-me": {
+		content: ``,
+	},
+};
 
 export default async function Page(props: {
-  params: Promise<{ segments?: string[] }>
+	params: Promise<{ segments?: string[] }>;
 }) {
-  const params = await props.params
-  if (!params.segments) {
-    return notFound()
-  }
+	const params = await props.params;
+	if (!params.segments) {
+		return notFound();
+	}
 
-  const path = `/${params.segments.join('/')}`
-  const page = pages[path]
+	const path = `/${params.segments.join("/")}`;
+	const page = pages[path];
 
-  if (!page) {
-    return notFound()
-  }
+	if (!page) {
+		return notFound();
+	}
 
-  return (
-    <div className="prose pb-8 pt-4 lg:prose-lg xl:prose-xl">
-      <MDXRemote
-        source={page.content}
-        components={{
-          a: (props) => <Link {...(props as LinkProps)} />
-        }}
-      />
-    </div>
-  )
+	return (
+		<div className="prose pb-8 pt-4 lg:prose-lg xl:prose-xl">
+			<MDXRemote
+				source={page.content}
+				components={{
+					a: (props) => <Link {...(props as LinkProps)} />,
+				}}
+			/>
+		</div>
+	);
 }
--- src/ui/footer/footer.tsx
+++ src/ui/footer/footer.tsx
@@ -1,76 +1,73 @@
-import { getTranslations } from '@/i18n/server'
-import StoreConfig from '@/store.config'
-import { Newsletter } from '@/ui/footer/newsletter.client'
-import { YnsLink } from '@/ui/yns-link'
+import { getTranslations } from "@/i18n/server";
+import StoreConfig from "@/store.config";
+import { Newsletter } from "@/ui/footer/newsletter.client";
+import { YnsLink } from "@/ui/yns-link";
 
 const sections = [
-  {
-    header: 'Products',
-    links: StoreConfig.categories.map(({ name, slug }) => ({
-      label: name,
-      href: `/category/${slug}`
-    }))
-  },
-  {
-    header: 'Support',
-    links: [
-      {
-        label: 'Features',
-        href: 'https://yournextstore.com/#features'
-      },
-      {
-        label: 'Pricing',
-        href: 'https://yournextstore.com/#pricing'
-      },
-      {
-        label: 'Contact Us',
-        href: 'mailto:hi@yournextstore.com'
-      }
-    ]
-  }
-]
+	{
+		header: "Products",
+		links: StoreConfig.categories.map(({ name, slug }) => ({
+			label: name,
+			href: `/category/${slug}`,
+		})),
+	},
+	{
+		header: "Support",
+		links: [
+			{
+				label: "Features",
+				href: "https://yournextstore.com/#features",
+			},
+			{
+				label: "Pricing",
+				href: "https://yournextstore.com/#pricing",
+			},
+			{
+				label: "Contact Us",
+				href: "mailto:hi@yournextstore.com",
+			},
+		],
+	},
+];
 
 export async function Footer() {
-  const t = await getTranslations('Global.footer')
+	const t = await getTranslations("Global.footer");
 
-  return (
-    <footer className="w-full bg-neutral-50 p-6 text-neutral-800 md:py-12">
-      <div className="container flex max-w-7xl flex-row flex-wrap justify-center gap-16 text-sm sm:justify-between">
-        <div className="">
-          <div className="flex w-full max-w-sm flex-col gap-2">
-            <h3 className="font-semibold">{t('newsletterTitle')}</h3>
-            <Newsletter />
-          </div>
-        </div>
+	return (
+		<footer className="w-full bg-neutral-50 p-6 text-neutral-800 md:py-12">
+			<div className="container flex max-w-7xl flex-row flex-wrap justify-center gap-16 text-sm sm:justify-between">
+				<div className="">
+					<div className="flex w-full max-w-sm flex-col gap-2">
+						<h3 className="font-semibold">{t("newsletterTitle")}</h3>
+						<Newsletter />
+					</div>
+				</div>
 
-        <nav className="grid grid-cols-2 gap-16">
-          {sections.map((section) => (
-            <section key={section.header}>
-              <h3 className="mb-2 font-semibold">{section.header}</h3>
-              <ul role="list" className="grid gap-1">
-                {section.links.map((link) => (
-                  <li key={link.label}>
-                    <YnsLink
-                      className="underline-offset-4 hover:underline"
-                      href={link.href}
-                    >
-                      {link.label}
-                    </YnsLink>
-                  </li>
-                ))}
-              </ul>
-            </section>
-          ))}
-        </nav>
-      </div>
-      <div className="container mt-8 flex max-w-7xl flex-col items-center justify-between gap-4 text-sm text-neutral-500 md:flex-row">
-        <div>
-          <p>
-            © {new Date().getFullYear()} - {process.env.NEXT_PUBLIC_APP_TITLE}
-          </p>
-          <p>{process.env.NEXT_PUBLIC_APP_SUBTITLE}</p>
-        </div>
-      </div>
-    </footer>
-  )
+				<nav className="grid grid-cols-2 gap-16">
+					{sections.map((section) => (
+						<section key={section.header}>
+							<h3 className="mb-2 font-semibold">{section.header}</h3>
+							<ul role="list" className="grid gap-1">
+								{section.links.map((link) => (
+									<li key={link.label}>
+										<YnsLink className="underline-offset-4 hover:underline" href={link.href}>
+											{link.label}
+										</YnsLink>
+									</li>
+								))}
+							</ul>
+						</section>
+					))}
+				</nav>
+			</div>
+			<div className="container mt-8 flex max-w-7xl flex-col items-center justify-between gap-4 text-sm text-neutral-500 md:flex-row">
+				<div>
+					<p>
+						© {new Date().getFullYear()} - {process.env.NEXT_PUBLIC_APP_TITLE}
+					</p>
+					<p>{process.env.NEXT_PUBLIC_APP_SUBTITLE}</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
